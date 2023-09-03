67d34f1be42905042b80f98dfa29e2b84b2f370c
Sabrina Rizzo <sabri.rizzo@gmail.com>
2023-09-03 11:43:10 +0200

    creacion de proyecto con srpingboot

---
+++ src/main/java/sabrina/rover/RoverApplication.java
@@ -0,0 +1,13 @@
+package sabrina.rover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoverApplication.class, args);
+	}
+
+}
