8c10412175705a45cdb56983d4f6e791ccc49397
Sabrina Rizzo <sabri.rizzo@gmail.com>
2023-09-06 13:07:57 +0200

    manejar desde consola el rover

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Tablero tablero = new Tablero(10, 10); // Ajusta el tamaño del tablero según tus necesidades
         Rover rover = new Rover(0, 0, "Este"); // Puedes ajustar la orientación inicial si lo deseas
@@ -19,38 +19,46 @@ public class Main {
         // Verificar la nueva posición del jugador
         System.out.println("Nueva posición del rover: X=" + rover.getX() + ", Y=" + rover.getY());
 
+        boolean continuarMoviendo;
+        do {
+            continuarMoviendo = true;
+            System.out.println("Ingresa los comandos que desee  para mover al rover (F=HACIA DELANTE, B=HACIA ATRAS," +
+                    " R=GIRAR SU ORIENTACION HACIA LA DERECHA , L=GIRAR SU ORIENTACION HACIA LA IZQUIERDA). Si ha llegado a destino y desea salir " +
+                    "ingrese S:");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine().toUpperCase();
+            char[] comandos = input.toCharArray();
 
-        System.out.println("Ingresa los comandos que desee  para mover al rover (F=HACIA DELANTE, B=HACIA ATRAS, R=GIRAR SU ORIENTACION HACIA LA DERECHA , L=GIRAR SU ORIENTACION HACIA LA IZQUIERDA) :");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine().toUpperCase();
-        char[] comandos = input.toCharArray();
+            for (int i = 0; i <= comandos.length - 1; i++) {
+                String comando = Character.toString(comandos[i]);
+                switch (comando) {
+                    case "B":
+                        rover.mover("B");
+                        System.out.println("la nueva posicion es :  x = " + rover.getX() + " y = " + rover.getY());
+                        break;
+                    case "F":
+                        rover.mover("F");
+                        System.out.println("la nueva posicion es :  x = " + rover.getX() + " y = " + rover.getY());
+                        break;
+                    case "R":
+                        rover.cambiarOrientacion("R");
+                        System.out.println("la nueva orientacion es : " + rover.getOrientacion());
+                        break;
+                    case "L":
+                        rover.cambiarOrientacion("L");
+                        System.out.println("la nueva orientacion es : " + rover.getOrientacion());
+                        break;
+                    case "S":
+                        continuarMoviendo = false;
+                        System.out.println("FIN: HA LLEGADO A DESTINO");
+                        break;
+                    default:
+                        System.out.println("Entrada no válida.");
+                        break;
+                }
+            }
+        } while (continuarMoviendo);
 
-        for (int i=0; i<=comandos.length-1; i++){
-            String comando = Character.toString(comandos[i]);
-            switch (comando) {
-                case "B":
-                    rover.mover("B");
-                    System.out.println("la nueva posicion es :  x = " + rover.getX() + " y = " + rover.getY());
-                    break;
-                case "F":
-                    rover.mover("F");
-                    System.out.println("la nueva posicion es :  x = " + rover.getX() + " y = " + rover.getY());
-                    break;
-                case "R":
-                    rover.cambiarOrientacion("R");
-                    System.out.println("la nueva orientacion es : " + rover.getOrientacion());
-                    break;
-                case "L":
-                    rover.cambiarOrientacion("L");
-                    System.out.println("la nueva orientacion es : " + rover.getOrientacion());
-                    break;
-                default:
-                    System.out.println("Entrada no válida.");
-                    break;
-            };
-
-        }
     }
-
-    }
-
+}
+    
